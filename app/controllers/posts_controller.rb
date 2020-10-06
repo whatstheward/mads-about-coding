25b7bd9b9b67691527910d28eb0b9897978dadf7
github username <github email address>
2020-10-06 10:39:31 -0400

    filter by tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
 
     def index
+        if params[:ids]
+            @posts = Post.find(params[:ids])
+        else
+            @posts = Post.all
+        end
     end
 
     def filter
-        tag = Tag.find_by!(name: params[:q])
-        # @posts = Post.left_outer_joins(:tags).where('tag.name' => params[:q])
-        byebug
+        @posts = Post.includes(:tags).where(tags:{name:params[:q]})
+        redirect_to posts_path(ids:@posts.ids)
     end
 
     def show
