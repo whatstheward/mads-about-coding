78dc97f48decb599581eb5df6d3e256b6df61a18
github username <github email address>
2020-10-13 14:10:55 -0400

    fix tag links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,11 @@ class PostsController < ApplicationController
         @current = params[:page].to_i
         slice_idx = 0
         slice_idx = @current == "1" ? 0 : (@current.to_i-1) * 8 if params[:page].present?
-            @posts = Post.where(nil)
-            @page_count = Post.count % 8 == 0 ? Post.count/8 : Post.count/8+1
-            @posts = Post.offset(slice_idx).take(8)
-            @page_count = params[:ids].length > 8 ? 0 : params[:ids].length/8+1  if params[:ids].present?
-            @posts = Post.find(params[:ids]).slice(slice_idx, slice_idx+8) if params[:ids].present?
-            @posts = Post.search_by_keyword(params[:search]) if params[:search].present?
-            @page_count = @posts.count % 8 == 0 ? @posts.count/8 : @posts.count/8+1 if params[:search].present?
-    end
-
-    def filter
-        @posts = Post.includes(:tags).where(tags:{name:params[:q]})
-        redirect_to posts_path(ids:@posts.ids, page: 1)
+        @posts = Post.where(nil)
+        @posts = Post.search_by_keyword(params[:search]) if params[:search].present?
+        @posts = Post.search_by_tag(params[:q]) if params[:q].present?
+        @page_count = @posts.count % 8 == 0 ? @posts.count/8 : @posts.count/8+1
+        @posts = @posts.offset(slice_idx).take(8)
     end
 
     def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     has_many :post_tags
     has_many :tags, through: :post_tags
     scope :search_by_keyword, -> (keyword){where("body ILIKE ? OR title ILIKE ?", "%#{keyword}%", "%#{keyword}%")}
+    scope :search_by_tag, -> (tag){includes(:tags).where(tags:{name:tag})}
 
     def tag_names
         self.tags.map do |tag|
